63fca7360897fa33b75dcc18d00d820c778fe6ed
Andrei Yeutukhou <gavnorock84@hotmail.com>
2024-02-26 20:34:45 +0300

    fix columns behavior

--- src/components/KanbanColumn.tsx
+++ src/components/KanbanColumn.tsx
@@ -58,32 +58,24 @@ const KanbanColumn: FC<Props> = ({ column, isLoading, onChange }) => {
       .fetchQuery({ queryKey: ['todo'], queryFn: () => updateTodo(todo) })
       .then((todoTask) => {
           const hasNewStatus = column.todos.find(({ id }) => id === todo.id)?.status !== todo.status
-
-          onChange((prevColumns) =>
-            prevColumns.map((currentColumn) =>
-              currentColumn.id === todoTask.status
-                ? {
-                  ...currentColumn, todos: currentColumn.todos.map((task) =>
-                    task.id === todoTask.id
-                      ? todoTask
-                      : task
-                  )
-                }
-                : { ...currentColumn, todos: currentColumn.todos.filter((task) => task.id !== todoTask.id) }
+          hasNewStatus
+            ? onChange((prevColumns) =>
+              prevColumns.map((currentColumn) =>
+                currentColumn.id === todoTask.status
+                  ? { ...currentColumn, todos: [todoTask, ...currentColumn.todos] }
+                  : { ...currentColumn, todos: currentColumn.todos.filter((task) => task.id !== todoTask.id) }
+              ))
+            : onChange((prevColumns) =>
+              prevColumns.map((currentColumn) =>
+                currentColumn.id === todoTask.status
+                  ? {
+                    ...currentColumn, todos: currentColumn.todos.map((task) =>
+                      task.id === todoTask.id ? todoTask : task
+                    )
+                  }
+                  : { ...currentColumn, todos: currentColumn.todos.filter((task) => task.id !== todoTask.id) }
+              )
             )
-          )
-
-          // onChange((prevColumns) =>
-          //  prevColumns.map((currentColumn) =>
-          //    currentColumn.id !== todoTask.status
-          //      ? {
-          //        ...currentColumn, todos: currentColumn.todos.filter((task) => task.id !== todoTask.id
-          //        )
-          //      }
-          //      : currentColumn
-          //  ))
-
-          // console.log(hasNewStatus)
         }
       )
   }
